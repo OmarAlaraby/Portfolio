19fe1d51a0fb079ee4627f923aba1c66d36f1e45
OmarAlaraby <omar.alaraby23@gmail.com>
2025-04-05 14:33:05 +0200

    fixing allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://omar-alaraby-portfolio.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'omar-alaraby-portfolio.onrender.com',
+    '127.0.0.1', 
+    'localhost' 
+]
 
 
 # Application definition
@@ -143,7 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')

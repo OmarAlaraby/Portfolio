a5b3e12ef5e30b449b6a1b5aadcdd555107e40a4
Omar Alaraby <99359641+OmarAlaraby@users.noreply.github.com>
2025-04-06 15:46:25 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,12 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "omar-alaraby-portfolio.onrender.com", 
-    "0.0.0.0",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

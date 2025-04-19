6f5f4feca854b12f4e277fbff15348bb3df54e4c
Omar Alaraby <99359641+OmarAlaraby@users.noreply.github.com>
2025-04-19 22:54:35 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,10 @@ set -e
 export DEBUG=False
 export PYTHONUNBUFFERED=1
 
+# Start Gunicorn
 echo "Starting Gunicorn server..."
 exec poetry run gunicorn \
-    --bind 0.0.0.0:$PORT \ 
+    --bind 0.0.0.0:8000 \
     --workers 3 \
     --timeout 120 \
     --access-logfile - \
